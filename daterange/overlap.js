b13cd86de9dc666a818fa88d876d5ae07e2a8478
0xrfsd <64338100+0xrfsd@users.noreply.github.com>
2021-01-02 12:32:27 -0300

    Add files via upload

---
+++ daterange/overlap.js
@@ -0,0 +1,15 @@
+// Code from stackoverflow
+
+// if((StartDate1 <= EndDate2) && (StartDate2 <= EndDate1)) {
+  	//overlapping dates
+//}
+
+StartDate1 = 121212;
+EndDate1 = 131212;
+
+StartDate2 = 141212;
+EndDate2 = 151212;
+
+if((StartDate1 <= EndDate2) && (StartDate2 <= EndDate1)) {
+  	console.log(True);
+}
